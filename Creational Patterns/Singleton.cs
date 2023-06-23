756f3fc7f17a049f337e0820b14b0a90d8aefa4f
redfoxbg <redfoxbg@gmail.com>
2023-06-23 22:49:40 +0300

    Add project files.

---
+++ Creational Patterns/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Design_Patterns_with_C_.Creational_Patterns
+{
+    // DEFINITION : Ensure a class has only one instance, and provide a global point of access to it.
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance;
+        private static int totalInstances;
+
+        private Singleton()
+        {
+            Console.WriteLine("--Private constructor is called.");
+            Console.WriteLine("--Exit now from private constructor.");
+        }
+
+        static Singleton()
+        {
+            Console.WriteLine("-Static constructor is caled.");
+            instance = new Singleton();
+            totalInstances++;
+            Console.WriteLine($"-Singleton instance is created. Number of instances: {totalInstances}");
+            Console.WriteLine("-Exit from static constructor.");
+        }
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        //public static Singleton GetInstance => instance;
+
+        public static int MyInt = 25;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Design_Patterns_with_C_.Creational_Patterns;
+
+namespace Design_Patterns_with_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Singleton Pattern Demonstration.***\n");
+            //Console.WriteLine($"The value of MyInt is: {Singleton.MyInt}");
+
+            //Singleton s = new Singleton(); //error
+
+            Console.WriteLine("Trying to get a Singleton instance, called firstInstance.");
+
+            Singleton firstInstance = Singleton.GetInstance;
+
+            Console.WriteLine("Trying to get another Singleton instance, called secondInstance.");
+
+            Singleton secondInstance = Singleton.GetInstance;
+
+            if (firstInstance.Equals(secondInstance))
+            {
+                Console.WriteLine("The firstInstance and secondInstance are the same.");
+            }
+            else
+            {
+                Console.WriteLine("Different instance exist.");
+            }
+            Console.Read();
+        }
+    }
+}
