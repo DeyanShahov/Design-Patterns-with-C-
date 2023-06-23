f6cf398a92cb092b0f35ba4cf88890e7bc1c2179
redfoxbg <redfoxbg@gmail.com>
2023-06-24 01:41:23 +0300

    Adding Prototype pattern

---
+++ Creational Patterns/Prototype.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Design_Patterns_with_C_.Creational_Patterns
+{
+    //Definition: Provides an alternative method for instantiating new object by copying or cloning an instance of an existing object.
+
+    //Concept: Idea of this pattern is to create an object that is based on another object. This existing object acts as a template for the new object.
+    internal class Prototype
+    {
+    }
+
+
+    public abstract class BasicCar
+    {
+        public int basePrice = 0;
+        public int onRoadPrice = 0;
+        public string ModelName { get; set; }
+
+        public static int SetAdditionalPrice()
+        {
+            Random random = new Random();
+            int additionalPrice = random.Next(200000, 500000);
+            return additionalPrice;
+        }
+
+        public abstract BasicCar Clone();
+
+    }
+
+    public class Nano : BasicCar
+    {
+        public Nano(string m) 
+        {
+            ModelName = m;
+
+            basePrice = 100000;
+        }
+
+        public override BasicCar Clone()
+        {
+            return this.MemberwiseClone() as Nano;
+        }
+    }
+
+    public class Ford : BasicCar
+    {
+        public Ford(string m)
+        {
+            ModelName = m;
+            basePrice = 500000;
+        }
+
+        public override BasicCar Clone()
+        {
+            return this.MemberwiseClone() as Ford;
+        }
+    }
+
+
+    public class CarFactory
+    {
+        private readonly BasicCar nano, ford;
+
+        public CarFactory()
+        {
+            nano = new Nano("Green Nano");
+            ford = new Ford("Ford Yellow");
+        }
+
+        public BasicCar GetNano() => nano.Clone();
+
+        public BasicCar GetFord() => ford.Clone();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,61 @@
 ﻿using Design_Patterns_with_C_.Creational_Patterns;
+using System.Diagnostics;
 
 namespace Design_Patterns_with_C_
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+            // CallSingletonPatterns();
+            //CallPrototypePattern();
+            Call2PrototypePattern();
+        }
+
+        private static void Call2PrototypePattern()
+        {
+            Console.WriteLine("***Prototype Pattern Demo2.***\n");
+            CarFactory carFactory = new CarFactory();
+
+            // Get a Nano
+            BasicCar basicCar = carFactory.GetNano();
+            //Working on cloned copy
+            basicCar.onRoadPrice = basicCar.basePrice + BasicCar.
+           SetAdditionalPrice();
+            Console.WriteLine($"Car is: {basicCar.ModelName}, and it's price is Rs. {basicCar.onRoadPrice}");
+            // Get a Ford now
+            basicCar = carFactory.GetFord();
+            // Working on cloned copy
+            basicCar.onRoadPrice = basicCar.basePrice + BasicCar.
+           SetAdditionalPrice();
+            Console.WriteLine($"Car is: {basicCar.ModelName}, and it's price is Rs. {basicCar.onRoadPrice}");
+
+            Console.ReadLine();
+        }
+
+        private static void CallPrototypePattern()
+        {
+            Console.WriteLine("***Prototype Pattern Demo***\n");
+            BasicCar nano = new Nano("Green Nano");
+            BasicCar ford = new Ford("Ford Yellow");
+            BasicCar basicCar;
+
+            //Nano
+            basicCar = nano.Clone();
+            //Working on cloned copy
+            basicCar.onRoadPrice = basicCar.basePrice + BasicCar.SetAdditionalPrice();
+            Console.WriteLine($"Car is: {basicCar.ModelName}, and its price is Rs. {basicCar.onRoadPrice}");
+
+            //Ford
+            basicCar = ford.Clone();
+            //Working  on cloned copy
+            basicCar.onRoadPrice = basicCar.basePrice + BasicCar.SetAdditionalPrice();
+            Console.WriteLine($"Car is: {basicCar.ModelName}, and its price is Rs. {basicCar.onRoadPrice}");
+
+            Console.ReadLine();
+        }
+
+        private static void CallSingletonPatterns()
         {
             Console.WriteLine("***Singleton Pattern Demonstration.***\n");
             Console.WriteLine($"The value of MyInt is: {SingletonStaticConstructor.MyInt}");
@@ -27,6 +78,14 @@ namespace Design_Patterns_with_C_
             {
                 Console.WriteLine("Different instance exist.");
             }
+
+            Console.WriteLine();
+
+            SingletonDoubleCheckedLock thirdInstance = SingletonDoubleCheckedLock.GetInstance;
+            SingletonDoubleCheckedLock fourthInstance = SingletonDoubleCheckedLock.GetInstance;
+
+            if (thirdInstance.Equals(fourthInstance)) Console.WriteLine("The thirdInstance and fourthInstance are the same.");
+            else Console.WriteLine("Different instance exist.");
             Console.Read();
 
             //Console.WriteLine(Singleton.MyInt);
